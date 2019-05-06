10830f24939bbc4d08c873b272a625119c98f7a8
Melanie Sumner <melaniersumner@gmail.com>
2019-05-06 11:37:20 -0500

    initial commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('alpha');
+  this.route('bravo', function() {
+    this.route('november');
+  });
 });
 
 export default Router;
